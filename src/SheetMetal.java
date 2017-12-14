7d8f896e8d20e3fc4daff1d9767cbeb9f00c43db
Adam <adamferwerda@gmail.com>
2017-12-14 16:25:01 -0600

    Getters and Setters for Nut, Bolt, SheetMetal

---
+++ src/SheetMetal.java
@@ -0,0 +1,22 @@
+public class SheetMetal implements  Part{
+    
+    @Override
+    public double getCost() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void printBillOfMaterials() {
+
+    }
+}
